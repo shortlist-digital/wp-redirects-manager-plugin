631437decf20c7cdce7c5b67d2feb79faea9b0f8
Leo <leonoragilmour@sky.com>
2017-11-03 15:27:12 +0000

    init

---
+++ readme.md
@@ -0,0 +1,2 @@
+# WP Redirects Manager Plugin
+A plugin to add the role to enable editors to add redirect rules
---
+++ wp-redirects-manager-plugin.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* @wordpress-plugin
+* Plugin Name: WP Redirects Manager Plugin
+* Plugin URI: http://github.com/shortlist-digital/wp-redirects-manager-plugin
+* Description: A plugin to add the role to enable editors to add redirect rules
+* Version: 1.0.0
+* Author: Shortlist Studio
+* Author URI: http://shortlist.studio
+* License: MIT
+*/
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+class WpRedirectsManagerPlugin
+{
+    public function __construct()
+    {
+        //
+    }
+}
+new WpRedirectsManagerPlugin();
